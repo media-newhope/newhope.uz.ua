3658210a20213e1a77dd31bff679247159e81d6b
Anton Hocharov <a.goncharov@insart.com>
2025-03-24 11:14:54 +0200

    adjust next config

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,9 @@ const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
   trailingSlash: true,
-  basePath: "/newhope.uz.ua",
   images: {
     unoptimized: true,
   },
-  assetPrefix: "/newhope.uz.ua",
 };
 
 export default nextConfig;
