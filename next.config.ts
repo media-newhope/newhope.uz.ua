e1d55dc2725d8ee84165fe91a8bafcfa131e7302
Anton Hocharov <a.goncharov@insart.com>
2025-03-24 11:29:07 +0200

    add basePath

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
+  trailingSlash: true,
+  basePath: "newhope.uz.ua",
   images: {
     unoptimized: true,
   },
+  assetPrefix: "newhope.uz.ua",
 };
 
 export default nextConfig;
